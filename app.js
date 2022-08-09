9a6305f508329d76c4ed5dcf90591f14c0ae958d
İçimi Demirağ <icimidemirag@gmail.com>
2022-08-09 15:08:02 +0300

    Done odev8

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 5000;
