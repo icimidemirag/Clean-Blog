55d5755a8a8fcac8209e8793b642130016a13d65
İçimi Demirağ <icimidemirag@gmail.com>
2022-08-10 15:06:33 +0300

    Done odev9

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -21,6 +34,17 @@ app.get('/add_post', (req, res) => {
 });
 app.get('/post', (req, res) => {
   res.render('post');
+});
+app.get('/post/:id', async(req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('post',{
+    post
+  });
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 5000;
