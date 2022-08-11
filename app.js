d700ddff8647fa7641ede0f2064d5319a26a1d35
İçimi Demirağ <icimidemirag@gmail.com>
2022-08-11 14:38:16 +0300

    Done odev11

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const Post = require('./models/Post');
+const postController = require('./controllers/postController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -18,34 +20,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {
-    posts,
-  });
-});
-app.get('/post/:id', async(req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post',{
-    post
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-app.get('/post', (req, res) => {
-  res.render('post');
-});
+app.get('/', postController.getAllPosts);
+app.get('/post/:id', postController.getPost);
+app.post('/posts', postController.createPost);
+app.put('/post/:id', postController.updatePost);
+app.delete('/post/:id', postController.deletePost);
 
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPostPage);
+app.get('/post/edit/:id', pageController.getEditPage);
 
 const port = 5000;
 app.listen(port, () => {
---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+const Post = require('../models/Post');
+
+exports.getAllPosts = async (req, res) => {
+  const posts = await Post.find({}).sort('-dateCreated');
+  res.render('index', {
+    posts,
+  });
+};
+
+exports.getPost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post', {
+    post,
+  });
+};
+
+exports.updatePost = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.detail = req.body.detail;
+  post.save();
+
+  res.redirect(`/post/${req.params.id}`);
+};
+
+exports.createPost = async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+};
+
+exports.deletePost = async (req, res) => {
+  await Post.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+};
