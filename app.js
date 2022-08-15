f6576b0d86586de0a8c61d10bf5f27c4651e175a
İçimi Demirağ <74818535+icimidemirag@users.noreply.github.com>
2022-08-15 13:34:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/about', pageController.getAboutPage);
 app.get('/add_post', pageController.getAddPostPage);
 app.get('/post/edit/:id', pageController.getEditPage);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
